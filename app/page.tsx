2be453a8fc160b46fcc1281e773d09348abf14e5
Ken Mwangi <kenmwang071@gmail.com>
2023-07-19 10:27:08 +0300

    credentials

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="bg-rose-50/90 text-black">Home</h1>
+    </div>
+  );
 }
