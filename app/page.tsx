fde30a257ef0052befc9ad927debc0ff0c8be9e2
Ken Mwangi <kenmwang071@gmail.com>
2023-07-19 10:36:41 +0300

    getServersession

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import React from "react";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <div>
       <h1 className="bg-rose-50/90 text-black">Home</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </div>
   );
 }
