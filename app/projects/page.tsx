fe325f1597209dfd4373a4b3212c899726dd6019
Ken Mwangi <kenmwang071@gmail.com>
2023-07-25 15:40:00 +0300

    Projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="py-16 lg:py-20">
+      <Container>
+        <h2 className="h2">Projects</h2>
+      </Container>
+    </div>
+  );
+}
